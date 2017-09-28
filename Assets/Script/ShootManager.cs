a21b68ec75b9bd706230db88048f8184e0e851e5
ranzhai <zhairan2008@gmail.com>
2017-09-28 11:42:38 +0800

    add a scene

---
+++ Assets/Script/ShootManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootManager : MonoBehaviour {
+
+	public GameObject shotPrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Shoot(){
+
+		GameObject go = Instantiate (shotPrefab, Camera.main.transform.position, Camera.main.transform.rotation);
+		//go.transform.GetComponent<Rigidbody> ().AddForce (go.transform.forward * 5);
+
+	}
+}
